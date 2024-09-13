53ee5528ba0268aa2eab2800db825ecea3e326b0
jacekpoz <jacekpoz@proton.me>
2024-09-13 22:05:29 +0200

    add some more text to the main site

--- site/index.nix
+++ site/index.nix
@@ -28,6 +28,31 @@
             </li>
           '') webringMembers)}
         </ul>
+
+        ${h2 "about"}
+        <p>
+          this is a webring for people passionate about <a href="https://nix.dev/">nix</a>/<a href="https://nixos.org/">os</a>
+          <br>
+        </p>
+
+        ${h2 "joining"}
+        <p>
+          to join, have a personal website (bonus points if it uses nix!) and add the following links to it (they have to be on the main page):
+          <br>
+          webring site: <code>https://nixwebr.ing</code>
+          <br>
+          next site: <code>https://nixwebr.ing/next/&lt;name&gt;</code>
+          <br>
+          previous site: <code>https://nixwebr.ing/prev/&lt;name&gt;</code>
+          <br>
+          random site (optional): <code>https://nixwebr.ing/rand</code>
+          <br>
+          and make a PR to one of <a href="https://github.com/jacekpoz/nixwebr.ing">the</a> <a href="https://codeberg.org/jacekpoz/nixwebr.ing">repos</a>
+          <br>
+          feel free to also link your nixos configuration in the PR!
+          <br>
+          if you don't know how to / can't make a PR for some reason feel free to <a href="https://jacekpoz.pl">contact me</a>, I can add you to the webring myself
+        </p>
       </body>
     </html>
   '';
