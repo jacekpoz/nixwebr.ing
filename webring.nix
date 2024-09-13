90bff96fd86838c554f031613c7e9803784c5bbc
Party Wumpus <48649272+PartyWumpus@users.noreply.github.com>
2024-09-14 00:41:41 +0100

    add party wumpus to webring

--- webring.nix
+++ webring.nix
@@ -1,4 +1,5 @@
 [
     { name = "poz"; domain = "jacekpoz.pl"; config = "flake.jacekpoz.pl"; }
     { name = "krizej"; domain = "krizej.codeberg.page"; config = "git.jacekpoz.pl/krizej/nix"; }
+    { name = "partywumpus"; domain = "partywump.us"; config = "nix.partywump.us"; }
 ]
