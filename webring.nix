923df499e439b7ee58c579696e5d1ca5aa4f8fc1
jacekpoz <jacekpoz@proton.me>
2024-09-14 00:05:38 +0200

    add krizej's flake

--- webring.nix
+++ webring.nix
@@ -1,4 +1,4 @@
 [
     { name = "poz"; domain = "jacekpoz.pl"; config = "flake.jacekpoz.pl"; }
-    { name = "krizej"; domain = "krizej.codeberg.page"; }
+    { name = "krizej"; domain = "krizej.codeberg.page"; config = "git.jacekpoz.pl/krizej/nix"; }
 ]
