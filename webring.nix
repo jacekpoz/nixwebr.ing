e1b8a39aafcbe294eab398a35d1801de56443119
jacekpoz <jacekpoz@proton.me>
2024-09-15 10:39:59 +0200

    add notashelf to webring
    welcome to the ring babe :3

--- webring.nix
+++ webring.nix
@@ -2,4 +2,5 @@
     { name = "poz"; domain = "jacekpoz.pl"; config = "flake.jacekpoz.pl"; }
     { name = "krizej"; domain = "krizej.codeberg.page"; config = "git.jacekpoz.pl/krizej/nix"; }
     { name = "partywumpus"; domain = "partywump.us"; config = "nix.partywump.us"; }
+    { name = "notashelf"; domain = "notashelf.dev"; config = "github.com/NotAShelf/nyx"; }
 ]
