7354de83738dc4c2d5a5e2a764525df177091939
twig. <106347250+ImNotTwig@users.noreply.github.com>
2024-09-15 08:29:14 +0000

    add twig to webring

--- webring.nix
+++ webring.nix
@@ -2,4 +2,5 @@
     { name = "poz"; domain = "jacekpoz.pl"; config = "flake.jacekpoz.pl"; }
     { name = "krizej"; domain = "krizej.codeberg.page"; config = "git.jacekpoz.pl/krizej/nix"; }
     { name = "partywumpus"; domain = "partywump.us"; config = "nix.partywump.us"; }
+    { name = "twig"; domain = "imnottwig.github.io"; config = "git.disroot.org/twigthecat/nixos"; }
 ]
