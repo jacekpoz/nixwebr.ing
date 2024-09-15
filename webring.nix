e8a81c73c463956f67b42c10e38598b4f468bf4c
twig. <106347250+ImNotTwig@users.noreply.github.com>
2024-09-15 08:49:54 +0000

    add twig to webring (#3)
    Co-authored-by: poz <jacekpoz@proton.me>

--- webring.nix
+++ webring.nix
@@ -3,4 +3,5 @@
     { name = "krizej"; domain = "krizej.codeberg.page"; config = "git.jacekpoz.pl/krizej/nix"; }
     { name = "partywumpus"; domain = "partywump.us"; config = "nix.partywump.us"; }
     { name = "notashelf"; domain = "notashelf.dev"; config = "github.com/NotAShelf/nyx"; }
+    { name = "twig"; domain = "imnottwig.github.io"; config = "git.disroot.org/twigthecat/nixos"; }
 ]
