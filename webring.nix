ca825d6c51e9a58b52a54119a6b3b738271f4ef4
NotAShelf <raf@notashelf.dev>
2024-11-02 00:07:47 +0300

    revert `webring: remove notashelf`
    https://github.com/jacekpoz/nixwebr.ing/pull/14

--- webring.nix
+++ webring.nix
@@ -2,6 +2,7 @@
     { name = "poz"; site = "https://jacekpoz.pl"; config = "https://flake.jacekpoz.pl"; }
     { name = "krizej"; site = "https://krizej.codeberg.page"; config = "https://git.jacekpoz.pl/krizej/nix"; }
     { name = "partywumpus"; site = "https://partywump.us"; config = "https://nix.partywump.us"; }
+    { name = "notashelf"; site = "https://notashelf.dev"; config = "https://github.com/NotAShelf/nyx"; }
     { name = "twig"; site = "https://imnottwig.github.io"; config = "https://git.disroot.org/twigthecat/nixos"; }
     { name = "fufexan"; site = "https://fufexan.net"; config = "https://dots.fufexan.net"; }
     { name = "orangc"; site = "https://orangc.xyz"; config = "https://github.com/orxngc/dots"; }
