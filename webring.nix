9216d74a63d8e9e4e345f2975e101e6165890e4c
orangc <orangc@proton.me>
2024-11-04 12:48:26 +0300

    chore(webring): update github username

--- webring.nix
+++ webring.nix
@@ -5,7 +5,7 @@
     { name = "notashelf"; site = "https://notashelf.dev"; config = "https://github.com/NotAShelf/nyx"; }
     { name = "twig"; site = "https://imnottwig.github.io"; config = "https://git.disroot.org/twigthecat/nixos"; }
     { name = "fufexan"; site = "https://fufexan.net"; config = "https://dots.fufexan.net"; }
-    { name = "orangc"; site = "https://orangc.xyz"; config = "https://github.com/orxngc/dots"; }
+    { name = "orangc"; site = "https://orangc.xyz"; config = "https://github.com/orangci/dots"; }
     { name = "ayats"; site = "https://ayats.org/experiments"; config = "https://github.com/viperML/dotfiles"; }
     { name = "ajaxbits"; site = "https://ajaxbits.com"; config = "https://github.com/ajaxbits/config"; }
     { name = "jappie"; site = "https://jappie.dev"; config = "https://app.radicle.xyz/nodes/git.jappie.dev/rad:z4HEZGDPknT12W4fuXc6wM3HtYTf2"; }
